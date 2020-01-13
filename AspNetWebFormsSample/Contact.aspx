23c5f8ac65d66b19ffda1afd076ff2f939914499
Kirk Davis <kirkdavi@amazon.com>
2020-01-12 17:05:42 -0800

    Initial commit of existing solution

---
+++ AspNetWebFormsSample/Contact.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="Contact" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="AspNetWebFormsSample.Contact" %>
+<%@ Import Namespace="System.Configuration" %>
+<%@ Import Namespace="System.Web.Configuration" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>Your contact page.</h3>
+    <address>
+        One Microsoft Way<br />
+        Redmond, WA 98052-6399<br />
+        <abbr title="Phone">P:</abbr>
+        425.555.0100
+    </address>
+
+    TestKey = 
+    <%=(ConfigurationManager.AppSettings["TestKey"]) %>
+    <br />
+    TwitterKey = 
+    <%=(WebConfigurationManager.AppSettings["TwitterKey"]) %>
+    <br />
+    ConnectionString = 
+    <%=(ConfigurationManager.ConnectionStrings["DefaultConnection"]) %>
+
+
+    <address>
+        <strong>Support:</strong>   <a href="mailto:Support@example.com">Support@example.com</a><br />
+        <strong>Marketing:</strong> <a href="mailto:Marketing@example.com">Marketing@example.com</a>
+    </address>
+
+</asp:Content>
---
+++ AwsSsmConfigBuilder/AwsSsmConfigBuilder.cs
@@ -0,0 +1,70 @@
+﻿using Amazon.SimpleSystemsManagement;
+using Amazon.SimpleSystemsManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Microsoft.Configuration.ConfigurationBuilders
+{
+    public class AwsSsmConfigBuilder : KeyValueConfigBuilder
+    {
+        private string BaseParameterPath = "/padnugapp/ApiKeys";
+        private static IAmazonSimpleSystemsManagement _client;
+
+        static AwsSsmConfigBuilder()
+        {
+            _client = new AmazonSimpleSystemsManagementClient();
+        }
+
+        public override void Initialize(string name, NameValueCollection config)
+        {
+            base.Initialize(name, config);
+
+            if (config["ssmPrefix"] == null)
+                return;
+
+            BaseParameterPath = config["ssmPrefix"];
+        }
+
+        public override ICollection<KeyValuePair<string, string>> GetAllValues(string prefix)
+        {
+            if (_client == null)
+                return null;
+
+            var request = new GetParametersByPathRequest
+            {
+                Path = $"{BaseParameterPath}/{prefix}",
+                WithDecryption = true,
+            };
+
+            var response = _client.GetParametersByPathAsync(request).Result;
+
+            var result = response.Parameters.ToDictionary(param => param.Name, param => param.Value, StringComparer.OrdinalIgnoreCase);
+
+            return result;
+        }
+
+        public override string GetValue(string key)
+        {
+            if (_client == null)
+                return null;
+
+            var request = new GetParameterRequest
+            {
+                Name = $"{BaseParameterPath}/{key}",
+                WithDecryption = true,
+            };
+
+            try
+            {
+                var response = _client.GetParameterAsync(request).Result;
+
+                return response.Parameter.Value;
+            } catch
+            {
+                return null;
+            }
+        }
+    }
+}
